4aa7ad1d68809e316d17a5aad3a87097c75ddba3
Oluwaseun Omisakin <adedijioluwaseun@yahoo.com>
2023-04-06 13:05:19 +0000

    Modify subtract game code

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,3 +1,4 @@
+
 // Wait for the document to load
 // Add event listener to capture it
 
@@ -125,7 +126,7 @@ function displayMultiplyQuestion(operand1, operand2){
 }
 
 function displayDivideQuestion(operand1, operand2){
-    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand1").textContent = operand1 * operand2;
     document.getElementById("operand2").textContent = operand2;
     document.getElementById("operator").textContent = "/";
 }
